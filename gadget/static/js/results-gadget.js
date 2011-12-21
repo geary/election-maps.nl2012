b98baa16c5e433c34c76b21084e11701c024d62b
Michael Geary <mg@mg.to>
2011-12-21 11:34:22 -0800

    Use Maps API 3.5 for now to work around panning bug

--- gadget/static/js/results-gadget.js
+++ gadget/static/js/results-gadget.js
@@ -3,7 +3,7 @@
 // See UNLICENSE or http://unlicense.org/ for public domain notice.
 
 opt.writeScript( 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery' + ( opt.debug ? '.js' : '.min.js' ) );
-opt.writeScript( 'http://maps.google.com/maps/api/js?sensor=false' );
+opt.writeScript( 'http://maps.google.com/maps/api/js?v=3.5&sensor=false' );
 
 opt.writeScript( opt.codeUrl + 'js/polygonzo.js', opt.nocache );
 opt.writeScript( opt.codeUrl + 'js/results-map.js', opt.nocache );
