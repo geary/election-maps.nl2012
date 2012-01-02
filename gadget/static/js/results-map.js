586ff1a96c0b412a8e94df3aaf8193f9eb517298
Michael Geary <mg@mg.to>
2012-01-01 18:12:24 -0800

    Make candidate names bold in header

--- gadget/static/js/results-map.js
+++ gadget/static/js/results-map.js
@@ -405,6 +405,7 @@ document.write(
 		'#legend { background-color:#EAF0FA; }',
 		'div.legend-candidate, div.legend-filler { float:left; border:1px solid #EAF0FA; padding:6px 4px 5px 5px; }',
 		'div.legend-filler { float:left; border-color:transparent; padding:6px 4px 5px 5px; }',
+		'div.legend-candidate { font-weight:bold; }',
 		'div.legend-candidate { width:18%; }',
 		'div.legend-candidate { cursor:pointer; }',
 		'div.legend-candidate.hover, div.legend-candidate.selected { border:1px solid #6FA8DC; }',
