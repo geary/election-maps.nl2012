b498ddd450ebe7fb02af37db7573c82bcb0c4307
Michael Geary <mg@mg.to>
2012-01-05 23:48:26 -0800

    Minor refactoring

--- scripts/import-shapes.py
+++ scripts/import-shapes.py
@@ -7,6 +7,10 @@ import pg
 import private
 
 
+database = 'usageo'
+schema = 'carto2010'
+	
+
 def cartoFileName( state, type ):
 	return os.path.join(
 		private.SHAPEFILE_PATH,
@@ -15,29 +19,52 @@ def cartoFileName( state, type ):
 
 
 def process():
-	database = 'usageo'
-	schema = 'carto2010'
-	
+	createDatabase()
+	db = openDatabase()
+	createSchema( db )
+	loadStates( db )
+	loadCounties( db )
+	loadCountySubdivisions( db )
+	createCountyCousub( db )
+	closeDatabase( db )
+
+
+def createDatabase():
 	print 'Creating database %s' % database
 	db = pg.Database( database='postgres' )
 	db.createGeoDatabase( database )
 	db.connection.close()
-	
+
+
+def openDatabase():
 	print 'Opening database %s' % database
-	db = pg.Database( database=database )
-	
+	return pg.Database( database=database )
+
+
+def closeDatabase( db ):
+	db.connection.commit()
+	db.connection.close()
+
+
+def createSchema( db ):
 	print 'Creating schema %s' % schema
 	db.createSchema( schema )
 	db.connection.commit()
-	
+
+
+def loadStates( db ):
 	zipfile = cartoFileName( 'us', '040' )
 	print 'Loading %s' % zipfile
 	db.loadShapefile( zipfile, private.TEMP_PATH, schema+'.state', True )
-	
+
+
+def loadCounties( db ):
 	zipfile = cartoFileName( 'us', '050' )
 	print 'Loading %s' % zipfile
 	db.loadShapefile( zipfile, private.TEMP_PATH, schema+'.county', True )
-	
+
+
+def loadCountySubdivisions( db ):
 	db.execute( 'SELECT state FROM %s.state ORDER BY state ASC;' %( schema ) )
 	create = True
 	for state, in db.cursor.fetchall():
@@ -45,7 +72,9 @@ def process():
 		print 'Loading %s' % zipfile
 		db.loadShapefile( zipfile, private.TEMP_PATH, schema+'.cousub', create )
 		create = False
+
 	
+def createCountyCousub( db ):
 	# CT, MA, NH, VT report votes by county subdivision ("town")
 	whereCousub = '''
 		( state = '09' OR state = '25' OR state = '33' OR state = '50' )
@@ -76,9 +105,6 @@ def process():
 		'schema': schema,
 		'whereCousub': whereCousub
 	}) )
-	
-	db.connection.commit()
-	db.connection.close()
 
 
 def main():
