0299e8881e8629eae260988fb471dc6a0692d8a0
Michael Geary <mg@mg.to>
2012-01-19 13:43:02 -0800

    Adjust highway color

--- gadget/static/js/results-map.js
+++ gadget/static/js/results-map.js
@@ -1692,7 +1692,7 @@ function formatLegendTable( cells ) {
 		},{
 			featureType: "road",
 			elementType: "geometry",
-			stylers: [ { lightness: 25 }, { saturation: -10 }, { visibility: "simplified" } ]
+			stylers: [ { lightness: 60 }, { saturation: 10 }, { visibility: "simplified" } ]
 		},{
 			featureType: "transit",
 			stylers: [ { visibility: "off" } ]
