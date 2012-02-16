a6d75888c916993b3c3a7db2f4aa94514498c1e2
Michael Geary <mg@mg.to>
2012-02-15 20:03:58 -0800

    Load 500k/5M/20M shapes into separate databases

--- scripts/import-shapes.py
+++ scripts/import-shapes.py
@@ -7,40 +7,42 @@ import pg
 import private
 
 
-database = 'usageo'
 schema = 'carto2010'
 
 fullGeom = 'full_geom'
 googGeom = 'goog_geom'
 
 
-def cartoFileName( state, type, version='00' ):
+def cartoFileName( resolution, state, type, version='00' ):
 	return os.path.join(
 		private.SHAPEFILE_PATH,
-		'gz_2010_%s_%s_%s_500k.zip' %( state, type, version )
+		'gz_2010_%s_%s_%s_%s.zip' %( state, type, version, resolution )
 	)
 
 
 def process():
-	createDatabase()
-	db = openDatabase()
-	#addSimplificationFunction( db )
-	createSchema( db )
-	loadStates( db )
-	loadCounties( db )
-	loadCongressional( db )
-	loadCountySubdivisions( db )
-	closeDatabase( db )
+	for resolution in ( '500k', '5m', '20m', ):
+		database = 'usageo_' + resolution
+		createDatabase( database)
+		db = openDatabase( database )
+		#addSimplificationFunction( db )
+		createSchema( db )
+		loadStates( db, database, resolution )
+		loadCounties( db, database, resolution )
+		if resolution == '500k':
+			loadCongressional( db, database, resolution )
+			loadCountySubdivisions( db, database, resolution )
+		closeDatabase( db )
 
 
-def createDatabase():
+def createDatabase( database ):
 	print 'Creating database %s' % database
 	db = pg.Database( database='postgres' )
 	db.createGeoDatabase( database )
 	db.connection.close()
 
 
-def openDatabase():
+def openDatabase( database):
 	print 'Opening database %s' % database
 	return pg.Database( database=database )
 
@@ -59,47 +61,43 @@ def createSchema( db ):
 	db.connection.commit()
 
 
-def loadCartoFile( db, state, kind, version, table, create=True ):
-	zipfile = cartoFileName( state, kind, version )
+def loadCartoFile( db, database, resolution, state, kind, version, table, create=True ):
+	zipfile = cartoFileName( resolution, state, kind, version )
 	table = schema + '.' + table
 	print 'Loading %s' % zipfile
 	db.loadShapefile(
-		zipfile, private.TEMP_PATH, table,
+		database, zipfile, private.TEMP_PATH, table,
 		'full_geom', '4269', 'LATIN1', create
 	)
 	db.addGoogleGeometry( table, fullGeom, googGeom )
 	db.indexGeometryColumn( table, googGeom )
 
 
-def loadStates( db ):
-	loadCartoFile( db, 'us', '040', '00', 'state' )
+def loadStates( db, database, resolution ):
+	loadCartoFile( db, database, resolution, 'us', '040', '00', 'state' )
 
 
-def loadCounties( db ):
-	loadCartoFile( db, 'us', '050', '00', 'county' )
+def loadCounties( db, database, resolution ):
+	loadCartoFile( db, database, resolution, 'us', '050', '00', 'county' )
 
 
-def loadCongressional( db ):
-	loadForStates( db, '500', '11', 'cd', [ '20' ] )
+def loadCongressional( db, database, resolution ):
+	loadForStates( db, database, resolution, '500', '11', 'cd', [ '20' ] )
 
 
-def loadCountySubdivisions( db ):
-	loadForStates( db, '060', '00', 'cousub', [ '09', '25', '33', '50' ] )
+def loadCountySubdivisions( db, database, resolution ):
+	loadForStates( db, database, resolution, '060', '00', 'cousub', [ '09', '25', '33', '50' ] )
 
 
-def loadForStates( db, code, version, table, states=None ):
+def loadForStates( db, database, resolution, code, version, table, states=None ):
 	#if states is None:
 	#	db.execute( 'SELECT state FROM %s.state ORDER BY state ASC;' %( schema ) )
 	#	states = db.cursor.fetchall()
 	#...for state, in states:
 	create = True
 	for state in states:
-		loadCartoFile( db, state, code, version, table, create )
+		loadCartoFile( db, database, resolution, state, code, version, table, create )
 		create = False
-
-
-#def loadNH( db ):
-#	loadCartoFile( db, '33', '060', '00', 'gop2012' )
 
 
 def main():
