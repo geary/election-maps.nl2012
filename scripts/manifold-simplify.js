14b924c09e6a46b591d75510ecfe6c9968e0b2c6
Michael Geary <mg@mg.to>
2012-02-22 21:03:33 -0800

    Create simplified state-only polys, use them in map

--- scripts/manifold-simplify.js
+++ scripts/manifold-simplify.js
@@ -28,13 +28,20 @@ function ImportSimplifyExport() {
 }
 
 function ForAllShapes( callback ) {
-	var tables = [ 'state', 'county' ];
-	var resolutions = [ '20m', '5m', '500k' ];
-	var tolerances = [ 256, 512, 1024, 2048, 4096, 8192, 16384 ];
-//	var tables = [ 'county' ];
-//	var resolutions = [ '20m' ];
-//	var tolerances = [ 16384 ];
+	var tables = [
+		/*'state',*/ 'state02', 'state15', 'state99'
+//		,
+//		/*'county',*/ 'county02', 'county15', 'county99'
+	];
+//	var resolutions = [ '20m', '5m', '500k' ];
+	var tolerances = [ 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536 ];
+//	var tables = [ 'state02'/*, 'state15', 'state99'*/ ];
+	var resolutions = [ '20m' ];
+//	var tolerances = [ 32768, 65536, 131072, 262144 ];
+//	var tolerances = [ 4096, 8192, 16384, 32768, 65536 ];
+//	var tolerances = [ 4096 ];
 	
+/*
 	for( var table, iTable = -1;  table = tables[++iTable]; ) {
 		for( var res, iRes = -1;  res = resolutions[++iRes]; ) {
 			var shpNameBase = S( 'us2012-', table, '-', res, '-' );
@@ -45,6 +52,18 @@ function ForAllShapes( callback ) {
 			}
 		}
 	}
+*/
+
+	ForEach( tables, function( table ) {
+		ForEach( resolutions, function( res ) {
+			var shpNameBase = S( 'us2012-', table, '-', res, '-' );
+			var shpNameFull = S( shpNameBase, 'full' );
+			ForEach( tolerances, function( tol ) {
+				var shpNameSimple = S( shpNameBase, tol );
+				callback( shpNameFull, shpNameSimple, tol );
+			});
+		});
+	});
 }
 
 function RemoveDrawing( shpName ) {
@@ -54,32 +73,75 @@ function RemoveDrawing( shpName ) {
 
 function ImportShape( shpName ) {
 	var imp = Document.NewImport( 'SHP' );
+	imp.ConvertPolicy = Manifold.ConvertAll;
 	imp.Import( ShapePath(shpName), PromptNone );
 }
 
+function CopyColumn( columnSetTo, columnSetFrom, name ) {
+	var columnFrom = GetByName( columnSetFrom, name );
+	var columnTo = columnSetTo.NewColumn();
+	columnTo.CodePage = columnFrom.CodePage;
+	columnTo.Name = columnFrom.Name;
+	columnTo.Size = columnFrom.Size;
+	columnTo.Type = columnFrom.Type;
+	columnSetTo.Add( columnTo );
+}
+
 function Simplify( shpNameFull, shpNameSimple, tolerance ) {
+	var colNames = 'GEO_ID,STATE,NAME,LSAD,CENSUSAREA,FULL_GEOM'.split(',');
+
 	var drawingFull = GetDrawing( shpNameFull );
 	var objectSetFull = drawingFull.ObjectSet;
+	var tableFull = drawingFull.OwnedTable;
+	var columnSetFull = tableFull.ColumnSet;
+	var recordSetFull = tableFull.RecordSet;
+	var geomSetFull = objectSetFull.GeomSet;
+
 	var drawingSimple = Document.NewDrawing(
 		DrawingName(shpNameSimple),
 		drawingFull.CoordinateSystem,
 		false
 	);
 	var objectSetSimple = drawingSimple.ObjectSet;
-	var geomsFull = objectSetFull.GeomSet;
-	var geomsSimple = geomsFull.NormalizeTopology( tolerance );
-	for( var i = 0, n = geomsSimple.Count;  i < n;  ++i ) {
-		var geom = geomsSimple( i );
+	var tableSimple = drawingSimple.OwnedTable;
+	var columnSetSimple = tableSimple.ColumnSet;
+	var recordSetSimple = tableSimple.RecordSet;
+	var geomSetSimple = geomSetFull.NormalizeTopology( tolerance );
+	
+	ForEach( colNames, function( name ) {
+		CopyColumn( columnSetSimple, columnSetFull, name );
+	});
+	
+	if( objectSetFull.Count != geomSetSimple.Count ) {
+		errors.push( S(
+			'Object set length mismatch: ', shpNameSimple
+		) );
+		return null;
+	}
+
+	// I am assuming that objectSetFull and geomSetSimple are in the same order!
+	for( var i = 0, n = geomSetSimple.Count;  i < n;  ++i ) {
+		var object = objectSetFull( i );
+		var geom = geomSetSimple( i );
 		try {
 			objectSetSimple.Add( geom );
 		}
 		catch( e ) {
 			errors.push( S(
-				'Invalid geometry: ', shpNameSimple, '[', i, ']'
+				e.name, ' (', e.description, '): ', shpNameSimple, '[', i, ']'
 			) );
 			return null;
 		}
+		
+		var recordFull =
+			GetByID( recordSetFull, object.ID );
+		var recordSimple =
+			GetByID( recordSetSimple, objectSetSimple.LastAdded.ID );
+		ForEach( colNames, function( name ) {
+			recordSimple.Data(name) = recordFull.Data(name);
+		});
 	}
+
 	return drawingSimple;
 }
 
@@ -89,6 +151,14 @@ function ExportShape( drawing, shpNameExport ) {
 	DeleteFolder( folder );
 	CreateFolder( folder );
 	exp.Export( drawing, path, PromptNone );
+}
+
+function GetByID( set, id ) {
+	return set( set.ItemByID(id) );
+}
+
+function GetByName( set, name ) {
+	return set( set.ItemByName(name) );
 }
 
 function GetDrawing( shpName ) {
@@ -131,4 +201,17 @@ function FSO() {
 
 function S() {
 	return Array.prototype.join.call( arguments, '' );
+}
+
+function ForEach( set, callback ) {
+	if( set instanceof Array ) {
+		for( var i = 0, n = set.length;  i < n;  ++i ) {
+			callback( set[i], i );
+		}
+	}
+	else {
+		for( var i = 0, n = set.Count;  i < n;  ++i ) {
+			callback( set(i), i );
+		}
+	}
 }
