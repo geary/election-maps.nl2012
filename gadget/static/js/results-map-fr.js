663fce75ce5f34dac5db10b4b246bdc415f95803
Michael Geary <mg@mg.to>
2012-04-20 10:41:53 -0700

    Update Google logo URL

--- gadget/static/js/results-map-fr.js
+++ gadget/static/js/results-map-fr.js
@@ -290,7 +290,7 @@ document.write(
 	//'</a>',
 	//'<a id="gop-logo" class="logo" target="_blank" href="http://www.nvgopcaucus.com/" title="', 'dataAttribTitleGOP'.T(), '">',
 	//'</a>',
-	'<a id="google-logo" class="logo" target="_blank" href="http://www.google.com/elections/ed/us/home" title="', 'googlePoliticsTitle'.T(), '">',
+	'<a id="google-logo" class="logo" target="_blank" href="http://www.google.fr/elections/ed/fr" title="', 'googlePoliticsTitle'.T(), '">',
 	'</a>',
 	'<div id="error" style="display:none;">',
 	'</div>',
