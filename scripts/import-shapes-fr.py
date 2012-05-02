7b597813e4e215ec47ac99e35276f992fca5177e
Michael Geary <mg@mg.to>
2012-05-02 00:05:51 -0700

    Add outlying islands, handle lots of special cases

--- scripts/import-shapes-fr.py
+++ scripts/import-shapes-fr.py
@@ -19,12 +19,14 @@ def process():
 	db = openDatabase( database )
 	#addSimplificationFunction( db )
 	createSchema( db )
+	# Insee tables
 	loadNationTable( db )
 	loadRegionTable( db )
 	loadDepartmentTable( db )
 	loadArrondTable( db )
 	loadCantonTable( db )
 	loadCommuneTable( db )
+	# GEOFLA and GADM shapefiles
 	loadDepartmentShapes( db )
 	loadCommuneShapes( db )
 	updateDepartmentNames( db )
@@ -61,8 +63,28 @@ def createSchema( db ):
 	db.connection.commit()
 
 
+# TODO: these are all the columns used in the Insee tables.
+# Use these to simplify the code?
+# ar varchar(1)
+# artmaj varchar(5)
+# artmin varchar(5)
+# canton varchar(2)
+# cdc varchar(1)
+# cheflieu varchar(5)
+# com varchar(3)
+# ct varchar(2)
+# dep varchar(3)
+# nation varchar(2)
+# ncc varchar(70)
+# nccenr varchar(70)
+# reg varchar(2)
+# region varchar(2)
+# tncc varchar(1)
+# typct varchar(1)
+
+
 def loadNationTable( db ):
-	loadTable( db,
+	loadInseeTable( db,
 		'france2012',
 		'nation, ncc, nccenr',
 		'''
@@ -74,7 +96,7 @@ def loadNationTable( db ):
 
 
 def loadRegionTable( db ):
-	loadTable( db,
+	loadInseeTable( db,
 		'reg2012',
 		'region, cheflieu, tncc, ncc, nccenr',
 		'''
@@ -88,7 +110,7 @@ def loadRegionTable( db ):
 
 
 def loadDepartmentTable( db ):
-	loadTable( db,
+	loadInseeTable( db,
 		'depts2012',
 		'region, dep, cheflieu, tncc, ncc, nccenr',
 		'''	
@@ -103,7 +125,7 @@ def loadDepartmentTable( db ):
 
 
 def loadArrondTable( db ):
-	loadTable( db,
+	loadInseeTable( db,
 		'arrond2012',
 		'region, dep, ar, cheflieu, tncc, artmaj, ncc, artmin, nccenr',
 		'''	
@@ -121,7 +143,7 @@ def loadArrondTable( db ):
 
 
 def loadCantonTable( db ):
-	loadTable( db,
+	loadInseeTable( db,
 		'canton2012',
 		'region, dep, ar, canton, typct, cheflieu, tncc, artmaj, ncc, artmin, nccenr',
 		'''	
@@ -141,7 +163,7 @@ def loadCantonTable( db ):
 
 
 def loadCommuneTable( db ):
-	loadTable( db,
+	loadInseeTable( db,
 		'comsimp2012',
 		'cdc, cheflieu, reg, dep, com, ar, ct, tncc, artmaj, ncc, artmin, nccenr',
 		'''	
@@ -161,13 +183,13 @@ def loadCommuneTable( db ):
 	)
 
 
-def loadTable( db, table, cols, columns ):
+def loadInseeTable( db, table, cols, columns ):
 	source = '../shapes/insee/%s.txt' %( table )
 	target = '%s/%s-utf8.txt' %( private.TEMP_PATH, table )
 	utf8 = file(source).read().decode('latin1').encode('utf8')
 	file( target, 'w' ).write( utf8 )
 	#cols = re.sub( r'\s*(\S+)\s+([^\n])+(\n)\s*', r'\1,', columns )
-	db.execute( '''
+	db.executeCommit( '''
 		CREATE TABLE %(table)s (
 			gid serial,
 			%(columns)s
@@ -184,8 +206,14 @@ def loadTable( db, table, cols, columns ):
 		'columns': columns,
 		'target': target,
 	}) )
-	db.connection.commit()
 	os.remove( target )
+
+
+def loadGadmShapes( db, loader ):
+	loader( db, 'SPM', '975', '1', '1' )
+	loader( db, 'WLF', '986', '2', '2' )
+	loader( db, 'PYF', '987', '0', 'local' )
+	loader( db, 'NCL', '988', '2', '2' )
 
 
 def loadDepartmentShapes( db ):
@@ -198,8 +226,38 @@ def loadDepartmentShapes( db ):
 		fullGeom, '4326', 'LATIN1', True,
 		'%s/DEPARTEMENT.shp' % zipfile
 	)
+	loadGadmShapes( db, loadGadmDepartment )
 	db.addGoogleGeometry( table, fullGeom, googGeom )
 	db.indexGeometryColumn( table, googGeom )
+
+
+def loadGadmDepartment( db, abbr, geoid, level, suffix ):
+	table = schema + '.departement'
+	srctable = table + '_' + abbr.lower()
+	zipfile = abbr + '_adm'
+	filename = '../shapes/gadm/%s.zip' % zipfile
+	print 'Loading %s' % filename
+	db.loadShapefile(
+		filename, private.TEMP_PATH, srctable,
+		fullGeom, '4326', 'LATIN1', True,
+		'%s0.shp' % zipfile
+	)
+	db.executeCommit('''
+		INSERT INTO %(table)s
+			SELECT nextval('%(table)s_gid_seq'),
+				'%(dept)s' AS id_geofla,
+				'%(dept)s' AS code_dept, name_local AS nom_dept,
+				'' AS code_chf, '' AS nom_chf,
+				'' AS x_chf_lieu, '' AS y_chf_lieu,
+				'' AS x_centroid, '' AS y_centroid,
+				'' AS code_reg, '' AS nom_region,
+				ST_SetSRID( full_geom, 4326 )
+			FROM %(srctable)s;
+	''' %({
+		'table': table,
+		'srctable': srctable,
+		'dept': geoid,
+	}) )
 
 
 def updateDepartmentNames( db ):
@@ -220,15 +278,15 @@ def updateDepartmentNames( db ):
 			ON france.depts2012(cheflieu);
 		
 		UPDATE france.departement
-		SET code_dept = '97', code_reg = '06'
+		SET code_dept = '976', code_reg = '06'
 		WHERE code_dept = '985';
 		
 		UPDATE france.departement
-		SET code_dept = 'Z' || substring( code_reg from 2 for 1 )
+		SET code_dept = code_dept || substring( code_reg from 2 for 1 )
 		WHERE code_dept = '97';
 		
 		UPDATE france.depts2012
-		SET dep = 'Z' || substring( region from 2 for 1 )
+		SET dep = dep || substring( region from 2 for 1 )
 		WHERE dep = '97';
 		
 		UPDATE france.departement
@@ -244,6 +302,18 @@ def updateDepartmentNames( db ):
 	db.connection.commit()
 
 
+def tweakCommuneSQL( sqlfile ):
+	sql = file( sqlfile ).read()
+	sql = re.sub(
+		'"code_dept" varchar\(2\)',
+		'"code_dept" varchar(3)',
+		sql, 1
+	)
+	newfile = re.sub( '\.sql$', '-tweak.sql', sqlfile )
+	file( newfile, 'w' ).write( sql )
+	return newfile
+
+
 def loadCommuneShapes( db ):
 	table = schema + '.commune'
 	def load( zipfile, suffix, srid, create=False ):
@@ -253,33 +323,74 @@ def loadCommuneShapes( db ):
 		db.loadShapefile(
 			filename, private.TEMP_PATH, srctable,
 			fullGeom, srid, 'LATIN1', True,
-			'%s/COMMUNES/COMMUNE.shp' % zipfile
+			'%s/COMMUNES/COMMUNE.shp' % zipfile,
+			tweaksql=tweakCommuneSQL
 		)
 		if create:
 			db.createLikeTable( table, srctable )
 			db.addGeometryColumn( table, fullGeom, 4326, True )
-		db.executeCommit('''
-			INSERT INTO %(table)s
-				SELECT nextval('%(table)s_gid_seq'),
-					id_geofla, code_comm, insee_com, nom_comm, statut,
-					x_chf_lieu, y_chf_lieu, x_centroid, y_centroid, z_moyen, superficie,
-					population, code_cant, code_arr, code_dept, nom_dept, code_reg,
-					nom_region,
-					ST_Transform( ST_SetSRID( full_geom, %(srid)s ), 4326 )
-				FROM %(srctable)s;
-		''' %({
-			'table': table,
-			'srctable': srctable,
-			'srid': srid,
-		}) )
+		if 1:
+			db.executeCommit('''
+				INSERT INTO %(table)s
+					SELECT nextval('%(table)s_gid_seq'),
+						id_geofla, code_comm, insee_com, nom_comm, statut,
+						x_chf_lieu, y_chf_lieu, x_centroid, y_centroid, z_moyen, superficie,
+						population, code_cant, code_arr, code_dept, nom_dept, code_reg,
+						nom_region,
+						ST_Transform( ST_SetSRID( full_geom, %(srid)s ), 4326 )
+					FROM %(srctable)s;
+			''' %({
+				'table': table,
+				'srctable': srctable,
+				'srid': srid,
+			}) )
 	load( 'GEOFLA_1-1_SHP_LAMB93_FR-ED111', 'fr', '2154', True )
 	load( 'GEOFLA_1-1_SHP_RGM04UTM38S_YT-ED111', 'yt', '32738' ) # '6892'
 	load( 'GEOFLA_1-1_SHP_RGR92UTM40S_RE-ED111', 're', '2975' )
 	load( 'GEOFLA_1-1_SHP_UTM20W84_GP-ED111', 'gp', '32620' )
 	load( 'GEOFLA_1-1_SHP_UTM20W84_MQ-ED111', 'mq', '32620' )
 	load( 'GEOFLA_1-1_SHP_UTM22RGFG95_GF-ED111', 'gf', '2972' )
+	loadGadmShapes( db, loadGadmCommune )
 	db.addGoogleGeometry( table, fullGeom, googGeom )
 	db.indexGeometryColumn( table, googGeom )
+
+
+def loadGadmCommune( db, abbr, geoid, level, suffix ):
+	# TODO: refactor with loadGadmDepartment
+	if level is None: return
+	table = schema + '.commune'
+	srctable = table + '_' + abbr.lower()
+	zipfile = abbr + '_adm'
+	filename = '../shapes/gadm/%s.zip' % zipfile
+	print 'Loading %s' % filename
+	db.loadShapefile(
+		filename, private.TEMP_PATH, srctable,
+		fullGeom, '4326', 'LATIN1', True,
+		'%s%s.shp' % ( zipfile, level )
+	)
+	db.executeCommit('''
+		INSERT INTO %(table)s
+			SELECT nextval('%(table)s_gid_seq'),
+				id_%(level)s AS id_geofla,
+				id_%(level)s AS code_comm,
+				id_%(level)s AS insee_com,
+				name_%(suffix)s AS nom_comm,
+				'' AS statut,
+				0 AS x_chf_lieu, 0 AS y_chf_lieu,
+				0 AS x_centroid, 0 AS y_centroid,
+				0 AS z_moyen, 0 AS superficie, 0 AS population,
+				'' AS code_cant, '' AS code_arr,
+				'%(dept)s' AS code_dept, '' AS nom_dept,
+				'' AS code_reg, '' AS nom_region,
+				ST_SetSRID( full_geom, 4326 )
+			FROM %(srctable)s;
+	''' %({
+		'table': table,
+		'srctable': srctable,
+		'dept': geoid,
+		'level': level,
+		'suffix': suffix,
+	}) )
 
 
 def updateCommuneNames( db ):
@@ -301,11 +412,11 @@ def updateCommuneNames( db ):
 			ON france.comsimp2012(com);
 		
 		UPDATE france.commune
-		SET code_dept = 'Z' || substring( code_reg from 2 for 1 )
+		SET code_dept = code_dept || substring( code_reg from 2 for 1 )
 		WHERE code_dept = '97';
 		
 		UPDATE france.comsimp2012
-		SET dep = 'Z' || substring( reg from 2 for 1 )
+		SET dep = dep || substring( reg from 2 for 1 )
 		WHERE dep = '97';
 		
 		UPDATE france.commune
@@ -318,111 +429,6 @@ def updateCommuneNames( db ):
 	''' % {
 		'fromWhere': fromWhere,
 	})
-	db.connection.commit()
-
-
-# TODO: refactor
-def makeGopLocalTable( db ):
-	# CT, MA, NH, VT report votes by county subdivision ("town")
-	whereCousub = '''
-		( state = '09' OR state = '25' OR state = '33' OR state = '50' )
-	'''
-	# ND reports votes by state house district
-	whereSHD = '''
-		( state = '38' )
-	'''
-	# AK, ME, and WY report statewide votes only
-	whereState = '''
-		( state = '02' OR state = '23' OR state = '56' )
-	'''
-	# PR is not reported in the primary
-	whereNone = '''
-		( state = '72' )
-	'''
-	db.createLikeTable( schema+'.gop2012loc', schema+'.cousub' )
-	db.execute( '''
-		INSERT INTO %(schema)s.gop2012loc
-			SELECT nextval('%(schema)s.gop2012loc_gid_seq'),
-				geo_id, state, county, '' AS cousub,
-				name, lsad, censusarea, full_geom, goog_geom
-			FROM %(schema)s.county
-			WHERE
-				NOT %(whereNone)s
-				AND NOT %(whereCousub)s
-				AND NOT %(whereSHD)s
-				AND NOT %(whereState)s;
-		INSERT INTO %(schema)s.gop2012loc
-			SELECT nextval('%(schema)s.gop2012loc_gid_seq'),
-				geo_id, state, county, cousub,
-				name, lsad, censusarea, full_geom, goog_geom
-			FROM %(schema)s.cousub
-			WHERE %(whereCousub)s;
-		INSERT INTO %(schema)s.gop2012loc
-			SELECT nextval('%(schema)s.gop2012loc_gid_seq'),
-				geo_id, state, '' AS county, '' AS cousub,
-				name, lsad, censusarea, full_geom, goog_geom
-			FROM %(schema)s.state
-			WHERE %(whereState)s;
-		INSERT INTO %(schema)s.gop2012loc
-			SELECT nextval('%(schema)s.gop2012loc_gid_seq'),
-				geo_id, state, sldl AS county, '' AS cousub,
-				name, lsad, censusarea, full_geom, goog_geom
-			FROM %(schema)s.shd
-			WHERE %(whereSHD)s;
-	''' %({
-		'schema': schema,
-		'whereNone': whereNone,
-		'whereCousub': whereCousub,
-		'whereSHD': whereSHD,
-		'whereState': whereState,
-	}) )
-	db.connection.commit()
-
-
-# TODO: refactor
-def makeGopNationalTable( db ):
-	# ND reports votes by state house district
-	# Commented out for now, showing ND statewide in national county view
-	whereSHD = '''
-		( FALSE ) -- ( state = '38' )
-	'''
-	# AK, ME, ND, and WY display statewide votes only in national county view
-	whereState = '''
-		( state = '02' OR state = '23' OR state = '38' OR state = '56' )
-	'''
-	# PR is not reported in the primary
-	whereNone = '''
-		( state = '72' )
-	'''
-	db.createLikeTable( schema+'.gop2012nat', schema+'.county' )
-	db.execute( '''
-		INSERT INTO %(schema)s.gop2012nat
-			SELECT nextval('%(schema)s.gop2012nat_gid_seq'),
-				geo_id, state, county,
-				name, lsad, censusarea, full_geom, goog_geom
-			FROM %(schema)s.county
-			WHERE
-				NOT %(whereNone)s
-				AND NOT %(whereSHD)s
-				AND NOT %(whereState)s;
-		INSERT INTO %(schema)s.gop2012nat
-			SELECT nextval('%(schema)s.gop2012nat_gid_seq'),
-				geo_id, state, '' AS county,
-				name, lsad, censusarea, full_geom, goog_geom
-			FROM %(schema)s.state
-			WHERE %(whereState)s;
-		INSERT INTO %(schema)s.gop2012nat
-			SELECT nextval('%(schema)s.gop2012nat_gid_seq'),
-				geo_id, state, sldl AS county,
-				name, lsad, censusarea, full_geom, goog_geom
-			FROM %(schema)s.shd
-			WHERE %(whereSHD)s;
-	''' %({
-		'schema': schema,
-		'whereNone': whereNone,
-		'whereSHD': whereSHD,
-		'whereState': whereState,
-	}) )
 	db.connection.commit()
 
 
