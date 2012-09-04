c3200fbc8ffbcb7e8a30948707208820fa7b7c1a
Michael Geary <mg@mg.to>
2012-09-04 11:08:58 -0700

    Update Google Elections URL for NL

--- gadget/static/js/results-map-nl.js
+++ gadget/static/js/results-map-nl.js
@@ -325,7 +325,7 @@ document.write(
 	//'</a>',
 	'<a id="anp-logo" class="logo" target="_blank" href="http://www.anp.nl/" title="', 'anpCopyright'.T(), '">',
 	'</a>',
-	'<a id="google-logo" class="logo" target="_blank" href="http://www.google.fr/elections/ed/fr" title="', 'googlePoliticsTitle'.T(), '">',
+	'<a id="google-logo" class="logo" target="_blank" href="http://www.google.nl/elections/ed/nl" title="', 'googlePoliticsTitle'.T(), '">',
 	'</a>',
 	'<div id="error" style="display:none;">',
 	'</div>',
